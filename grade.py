cc56a44b364697a263b12172f48a0e47ec4cbb37
itayl <itay.lisker@gmail.com>
2023-09-01 13:42:39 +0300

    first commit, basic classes and first prototype of MSH alg (shouldn't work properly)

---
+++ grade.py
@@ -0,0 +1,32 @@
+class Grade:
+    import teacher
+
+    def __init__(self, name: str, hours_per_subject: dict):
+        self.name = name
+        self.hours_per_subject = hours_per_subject
+        self.MSH = [  # MSH is short for: "Marehet SHaot"
+                    [None, None, None, None, None, None, None, None, None, None, None, None],  # sunday
+                    [None, None, None, None, None, None, None, None, None, None, None, None],  # monday
+                    [None, None, None, None, None, None, None, None, None, None, None, None],  # tuesday
+                    [None, None, None, None, None, None, None, None, None, None, None, None],  # wednesday
+                    [None, None, None, None, None, None, None, None, None, None, None, None],  # thursday
+                    [None, None, None, None, None, None, None, None, None, None, None, None]   # friday
+        ]
+
+    def change_hour(self, teacher: teacher, day: int, hour: int, action: str):
+
+        if action.lower == "remove":
+
+            self.hours_per_subject[self.MSH[day - 1][hour].subject] += 1
+            teacher.work_hours[day - 1][hour] = 1  # priority
+            self.MSH[day - 1][hour] = None
+
+        elif action.lower == "add":
+            self.MSH[day - 1][hour] = teacher
+            self.hours_per_subject[teacher.subject] -= 1
+            teacher.work_hours[day - 1][hour] = 0
+        else:
+            print('Invalid action')
+        # if action.lower == "change":
+        #     self.change_hour(teacher, day, hour, "remove")
+        #     self.change_hour(teacher, day, hour, "add")
---
+++ subject.py
@@ -0,0 +1,4 @@
+class Subject:
+    def __init__(self, name: str, hours_in_a_day: int):
+        self.hours_in_a_day = hours_in_a_day
+        self.name = name

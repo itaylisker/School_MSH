f2ddc905a24b702bd6b51823f3f74305409442ff
itayl <itay.lisker@gmail.com>
2023-10-15 02:28:15 +0300

    added the add grade button and fixed the Enum

--- common.py
+++ common.py
@@ -1,5 +1,14 @@
-import hashlib
+class Enum:
+    LOGIN_INFO = 'login'
+    ADD_SUBJECT = 'add_subject'
+    GET_SUBJECTS = 'get_subjects'
+    ADD_TEACHER = 'add_teacher'
+    GET_TEACHERS = 'get_teachers'
+    ADD_GRADE = 'add_grade'
 
 
+
+# TODO: think of an encryption that isn't hash
 def encode_password(password):
+    import hashlib
     return hashlib.md5(password.encode()).hexdigest()
